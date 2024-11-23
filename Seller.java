a2bd2933b0a0148b604b5945fc26b0d58f476981
William Koks <98177061+kokspro@users.noreply.github.com>
2024-11-22 21:23:12 -0600

    Add files via upload
    Everything is together, uniform, and working perfectly.  Just need Seller page and then some time to uniform it as well.

---
+++ Seller.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+
+public class Seller {
+	
+
+	@FXML
+	private Button login_button;
+	
+
+}
